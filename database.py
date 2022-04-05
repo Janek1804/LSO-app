f44a66bc1a7873121389ad899cd90dca343d6554
Janek1804 <jan.kalucki@szkola.salezjanie.com>
2022-04-05 22:01:39 +0200

    Initial commit

---
+++ database.py
@@ -0,0 +1,11 @@
+import  mysql.connector
+from datetime import datetime
+
+db_connect=mysql.connector.connect(user="root", password="root", database="count", host="localhost")
+db_cursor=db_connect.cursor()
+now = datetime.now()
+
+print("Current time=",now)
+
+if now.day==1:
+    cursor.execute(f"CREATE TABLE "
